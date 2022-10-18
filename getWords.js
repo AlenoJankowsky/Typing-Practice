f940ef0d1552cd6a539b90fc56a6d121db10aaaf
Aleno <aleno.jankowsky@5minds.de>
2022-10-18 16:17:17 +0200

    Add features for creating a text

--- getWords.js
+++ getWords.js
@@ -1,12 +1,33 @@
-async function getWord(webAPI) {
-  let x = await fetch(webAPI);
-  let y = await x.text();
-  document.getElementById('text').innerHTML = y;
-  let elements = document.getElementsByClassName('typing-area__text');
-  for (const element of elements) {
-    element.innerHTML = element.innerHTML.replace(/"/g,'');
-    element.innerHTML = element.innerHTML.replace(/\u005D/g,'');
-    element.innerHTML = element.innerHTML.replace(/\u005B/g,'');
-    element.innerHTML = element.innerHTML.replace(/,/g,' ');
-  }
+document.addEventListener('DOMContentLoaded', function() {
+  const generateTextButton = document.getElementById('generate-text-button');
+  
+  generateTextButton.addEventListener('click', async function() {
+    const amountOfWordsWanted = document.getElementById('amount-of-words').value;
+    const amountOfWordsWantedToString = amountOfWordsWanted.toString();
+    const webAPI = 'https://random-word-api.herokuapp.com/word?number=' + amountOfWordsWantedToString;
+    const fetchText = await fetch(webAPI);
+    const text = await fetchText.text();
+    const resultText = cleanText(text);
+    const paragraphWithText = document.getElementById('text');
+    for (let textIterator = 0; textIterator < resultText.length; textIterator++) {
+      paragraphWithText.innerHTML = resultText.slice(textIterator, textIterator + 1);
+      if (textIterator == 0) {
+        break;
+      }
+    }
+    
+    paragraphWithText.style.textDecoration = 'underline';
+    paragraphWithText.innerHTML = paragraphWithText.innerHTML + resultText.slice(1, resultText.length + 1);
+
+  });
+});
+
+function cleanText(text) {
+  const resultText = text
+    .replaceAll(',', " ")
+    .replaceAll('"', "")
+    .replaceAll('[', "")
+    .replaceAll(']', "");
+
+  return resultText;
 }
