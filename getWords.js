206be30ce125c8f9f37d650a55683e59f4266cf4
Aleno <aleno.jankowsky@5minds.de>
2022-10-28 15:57:08 +0200

    Add visual feature so that the user knows on which character they are

--- getWords.js
+++ getWords.js
@@ -1,5 +1,6 @@
 const generateTextButton = document.getElementById('generate-text-button');
 const paragraphWithText = document.getElementById('text');
+const inputButton = document.getElementById('generate-text-button');
 
 generateTextButton.addEventListener('click', async function() {
   let textArray = [];
@@ -14,18 +15,40 @@ generateTextButton.addEventListener('click', async function() {
     textArray.push(element);
   }
 
-  paragraphWithText.innerHTML = textArray;
-  document.addEventListener('keydown', function(event) {
-    paragraphWithText.innerHTML = textArray;
-    let charIterator = 0;
-    for (let loopIterator = 0; loopIterator < textArray.length; loopIterator++)
-      if (textArray[charIterator] == event.key) {
-        let span = document.createElement('span');
-        textArray[charIterator] = span[charIterator];
-        textArray[charIterator].style.color = 'green';
+  paragraphWithText.innerHTML = charArrayIntoString(textArray);
 
-        charIterator += 1;
-      }
+  var charIndex = 0;
+  paragraphWithText.innerHTML = markCurrentChar(paragraphWithText, charIndex);
+
+  inputButton.addEventListener('keydown', function(event) {
+    if (event.code == 'Space') {
+      charIndex += 1;
+      event.preventDefault();
+
+      return;
+    }
+  });
+
+  document.addEventListener('keydown', function(event) {
+    if (charIndex == textArray.length - 1) {
+      getRandomWords();
+    }
+
+    if (event.code == 'Space' && textArray[charIndex + 1] == " ") {
+      console.log("kek1");
+      charIndex += 1;
+      event.preventDefault();
+
+      return;
+    }
+
+    const userInputIsCorrect = textArray[charIndex] == event.key;
+    if (userInputIsCorrect) {
+      charIndex += 1;
+      paragraphWithText.innerHTML = markCurrentChar(paragraphWithText, charIndex);
+
+      return;
+    }
   });
 });
 
@@ -37,4 +60,39 @@ function cleanText(text) {
     .replaceAll(']', "");
 
   return resultText;
+}
+
+function charArrayIntoString(textArray) {
+  var resultString = "";
+  for (const char of textArray) {
+    resultString += char;
+  }
+
+  return resultString;
+}
+
+function markCurrentChar(paragraphText, charIndex) {
+  let span = document.createElement('span');
+  let textForSpan = document.createTextNode(paragraphText.innerText[charIndex]);
+  span.appendChild(textForSpan);
+  span.style.backgroundColor = 'lightgrey';
+  const resultParagraphText = paragraphText.innerText.substring(0, charIndex) + span.outerHTML + paragraphText.innerText.substring(charIndex + 1);
+
+  return resultParagraphText;
+}
+
+async function getRandomWords() {
+  let textArray = [];
+  const amountOfWordsWanted = document.getElementById('amount-of-words').value;
+  const amountOfWordsWantedToString = amountOfWordsWanted.toString();
+  const webAPI = 'https://random-word-api.herokuapp.com/word?number=' + amountOfWordsWantedToString;
+  const fetchText = await fetch(webAPI);
+  const text = await fetchText.text();
+  const resultText = cleanText(text);
+
+  for (const element of resultText) {
+    textArray.push(element);
+  }
+
+  paragraphWithText.innerHTML = charArrayIntoString(textArray);
 }
