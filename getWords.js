a30ecc0e2bdecf327fa25acc522efc9cacfa2922
Aleno <aleno.jankowsky@5minds.de>
2022-10-31 15:58:37 +0100

    Add experimental code

--- getWords.js
+++ getWords.js
@@ -1,6 +1,45 @@
 const generateTextButton = document.getElementById('generate-text-button');
-const paragraphWithText = document.getElementById('text');
+var paragraphWithText = document.getElementById('text');
 const inputButton = document.getElementById('generate-text-button');
+
+function cleanText(text) {
+  const resultText = text
+    .replaceAll(',', " ")
+    .replaceAll('"', "")
+    .replaceAll('[', "")
+    .replaceAll(']', "");
+
+  return resultText;
+}
+
+function charArrayIntoString(textArray) {
+  var resultString = "";
+  for (const char of textArray) {
+    resultString += char;
+  }
+
+  return resultString;
+}
+
+function markCurrentChar(paragraphWithText, charIndex) {
+  let span = document.createElement('span');
+  let textForSpan = document.createTextNode(paragraphWithText.innerText[charIndex]);
+  span.appendChild(textForSpan);
+  span.style.backgroundColor = 'white';
+  let resultParagraphText = paragraphWithText.innerText.substring(0, charIndex) + span.outerHTML + paragraphWithText.innerText.substring(charIndex + 1);
+
+  return resultParagraphText;
+}
+
+function markIncorrectChar(paragraphWithText, charIndex) {
+  let span = document.createElement('span');
+  let textForSpan = document.createTextNode(paragraphWithText.innerText[charIndex]);
+  span.appendChild(textForSpan);
+  span.style.color = 'red';
+  //let resultParagraphText = paragraphWithText.innerText.substring(0, charIndex) + span.outerHTML + paragraphWithText.innerText.substring(charIndex + 1);
+
+  return span.outerHTML;
+}
 
 generateTextButton.addEventListener('click', async function() {
   let textArray = [];
@@ -22,7 +61,6 @@ generateTextButton.addEventListener('click', async function() {
 
   inputButton.addEventListener('keydown', function(event) {
     if (event.code == 'Space') {
-      charIndex += 1;
       event.preventDefault();
 
       return;
@@ -30,15 +68,16 @@ generateTextButton.addEventListener('click', async function() {
   });
 
   document.addEventListener('keydown', function(event) {
-    if (charIndex == textArray.length - 1) {
-      getRandomWords();
-    }
+    console.log("kek");
 
     if (event.code == 'Space' && textArray[charIndex + 1] == " ") {
-      console.log("kek1");
       charIndex += 1;
       event.preventDefault();
 
+      return;
+    }
+
+    if (charIndex == textArray.length - 1) {
       return;
     }
 
@@ -46,53 +85,12 @@ generateTextButton.addEventListener('click', async function() {
     if (userInputIsCorrect) {
       charIndex += 1;
       paragraphWithText.innerHTML = markCurrentChar(paragraphWithText, charIndex);
+    }
 
-      return;
+    if (!userInputIsCorrect) {
+      let span = markIncorrectChar(paragraphWithText, charIndex);
+      console.log(span);
+      paragraphWithText.innerHTML = paragraphWithText.innerText.replace(paragraphWithText.innerText[charIndex], span);
     }
   });
 });
-
-function cleanText(text) {
-  const resultText = text
-    .replaceAll(',', " ")
-    .replaceAll('"', "")
-    .replaceAll('[', "")
-    .replaceAll(']', "");
-
-  return resultText;
-}
-
-function charArrayIntoString(textArray) {
-  var resultString = "";
-  for (const char of textArray) {
-    resultString += char;
-  }
-
-  return resultString;
-}
-
-function markCurrentChar(paragraphText, charIndex) {
-  let span = document.createElement('span');
-  let textForSpan = document.createTextNode(paragraphText.innerText[charIndex]);
-  span.appendChild(textForSpan);
-  span.style.backgroundColor = 'lightgrey';
-  const resultParagraphText = paragraphText.innerText.substring(0, charIndex) + span.outerHTML + paragraphText.innerText.substring(charIndex + 1);
-
-  return resultParagraphText;
-}
-
-async function getRandomWords() {
-  let textArray = [];
-  const amountOfWordsWanted = document.getElementById('amount-of-words').value;
-  const amountOfWordsWantedToString = amountOfWordsWanted.toString();
-  const webAPI = 'https://random-word-api.herokuapp.com/word?number=' + amountOfWordsWantedToString;
-  const fetchText = await fetch(webAPI);
-  const text = await fetchText.text();
-  const resultText = cleanText(text);
-
-  for (const element of resultText) {
-    textArray.push(element);
-  }
-
-  paragraphWithText.innerHTML = charArrayIntoString(textArray);
-}
