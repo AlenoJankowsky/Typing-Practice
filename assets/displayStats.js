9153269364936055c06773c89eb9d0d8abb60da9
Aleno <aleno.jankowsky@5minds.de>
2022-11-11 09:16:01 +0100

    Add variable to create more readability

--- assets/displayStats.js
+++ assets/displayStats.js
@@ -3,8 +3,8 @@ export function displayStats(userMistakesCount, userKeyTypeCount, seconds) {
     return 'CPM: ' + '0 ' + 'Wrong Chars: ' + '0%';
   }
 
-  seconds = seconds / 60;
-  const charactersPerMinute = userKeyTypeCount / seconds;
+  let minutes = seconds / 60;
+  const charactersPerMinute = userKeyTypeCount / minutes;
 
   return 'CPM: '+ Math.round(charactersPerMinute) + ' ' + 'Wrong Chars: ' + Math.round((userMistakesCount * 100 / userKeyTypeCount * 100) / 100) + '%';
 }
