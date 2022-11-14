749af19e699bca924dbb6cf72d0a315465fd9d37
Aleno <aleno.jankowsky@5minds.de>
2022-11-14 11:29:53 +0100

    Remove unnecessary multiplication of 100

--- assets/displayStats.js
+++ assets/displayStats.js
@@ -2,11 +2,11 @@ export function displayStats(userMistakesCount, userKeyTypeCount, seconds) {
   if (userMistakesCount == 0 && userKeyTypeCount == 0) {
     return 'CPM: ' + '0 ' + 'Wrong Chars: ' + '0%';
   }
-  
+
   let minutes = seconds / 60;
   const charactersPerMinute = userKeyTypeCount / minutes;
 
-  return 'CPM: '+ Math.round(charactersPerMinute) + ' ' + 'Wrong Chars: ' + Math.round((userMistakesCount * 100 / userKeyTypeCount * 100) / 100) + '%';
+  return 'CPM: '+ Math.round(charactersPerMinute) + ' ' + 'Wrong Chars: ' + Math.round((userMistakesCount / userKeyTypeCount) / 100) + '%';
 }
 
 export function displayTodayStats(userMistakesCount, userKeyTypeCount, seconds, amountOfSets) {
