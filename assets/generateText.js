d0599cb48050d47c4f64c624c36b27a6bf256c3f
Aleno <aleno.jankowsky@5minds.de>
2022-11-14 11:32:59 +0100

    Remove unnecessary parameter of charArray

--- assets/generateText.js
+++ assets/generateText.js
@@ -1,8 +1,8 @@
 import {charArrayIntoString} from  "./displayText.js";
 import {createIterableCharArray} from  "./createIterableCharArray.js"
 
-export async function generateText(paragraphWithText, charArray) {
-  charArray = await createIterableCharArray(paragraphWithText);
+export async function generateText(paragraphWithText) {
+  const charArray = await createIterableCharArray(paragraphWithText);
   paragraphWithText.innerHTML = charArrayIntoString(charArray);
 
   return charArray
