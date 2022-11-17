224a63455dfe26520071f945a812a80cbf5fbc2f
Aleno <aleno.jankowsky@5minds.de>
2022-11-17 09:03:36 +0100

    Add 'Enter' key for preventDefault

--- assets/preventDefault.js
+++ assets/preventDefault.js
@@ -1,5 +1,5 @@
 export function preventSpaceBar(event, charArray, charIndex) {
-  if (event.code == 'Space' && charArray[charIndex + 1] != " ") {
+  if (event.code == 'Space' && charArray[charIndex + 1] != " " || event.code == 'Enter') {
     event.stopPropagation();
     event.preventDefault();
 
