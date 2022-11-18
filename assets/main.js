7d27e95f773ded544cc3cfa7a1b008bd96a6fe05
Aleno <aleno.jankowsky@5minds.de>
2022-11-18 10:46:02 +0100

    Add full two dimensional array in a static way for key frequencies

--- assets/main.js
+++ assets/main.js
@@ -8,8 +8,9 @@ import {handleLocalStorage} from "./localStorageHandler.js";
 import {resetLocalStorageForTodayStats} from "./localStorageHandler.js";
 import {resetLocalStorageForTotalStats} from "./localStorageHandler.js";
 
-const keyboardKeysArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-const keyboardKeysArrayEntriesIterator = keyboardKeysArray.entries();
+const keyboardKeysArray = [['A', 0], ['B', 0], ['C', 0], ['D', 0], ['E', 0], ['F', 0], ['G', 0], ['H', 0], ['I', 0], ['J', 0], ['K', 0], ['L', 0], ['M', 0], ['N', 0], ['O', 0], ['P', 0], ['Q', 0],
+                           ['R', 0], ['S', 0], ['T', 0], ['U', 0], ['V', 0], ['W', 0], ['X', 0], ['Y', 0], ['Z', 0]];
+console.log(keyboardKeysArray);
 
 const generateTextButton = document.getElementById('generate-text-button');
 const resetProgressButton = document.getElementById('reset-button');
@@ -77,17 +78,11 @@ function resetProgress() {
   todayStatsText.innerHTML = displayTodayStats(userKeyTypeCount);
 }
 
-function initializeFrequencyEntries() {
-  keyboardKeysArray.forEach(function() {
-    for (let index of keyboardKeysArrayEntriesIterator) {
-      index = 0;
-    }
-  });
+function initializeFrequencyArray() {
+ 
 }
 
 generateTextButton.addEventListener('click', async function() {
-  initializeFrequencyEntries();
-
   if (generateTextButtonIsClicked) {
       resetProgress();
       document.removeEventListener('keydown', keyDownHandler);
@@ -108,7 +103,7 @@ generateTextButton.addEventListener('click', async function() {
       statsText.innerHTML = 'CPM: '+ Math.round(charactersPerMinute) + ' ' + 'Wrong Chars: ' + Math.round((userMistakesCount * 100 / userKeyTypeCount * 100) / 100) + '%';
     }
   }, 1000);
-  
+
   if (generateTextButtonIsClicked) {
     clearInterval(incrementSecondsInterval);
   }
