073a0b58722ddb5c46ea3cbaa49e72ac1b0c7451
Aleno <aleno.jankowsky@5minds.de>
2022-11-18 08:19:20 +0100

    Move functions in order to increase readability and fix styling issues concerning naming variables

--- assets/main.js
+++ assets/main.js
@@ -8,6 +8,9 @@ import {handleLocalStorage} from "./localStorageHandler.js";
 import {resetLocalStorageForTodayStats} from "./localStorageHandler.js";
 import {resetLocalStorageForTotalStats} from "./localStorageHandler.js";
 
+const keyboardKeysArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+const keyboardKeysArrayEntriesIterator = keyboardKeysArray.entries();
+
 const generateTextButton = document.getElementById('generate-text-button');
 const resetProgressButton = document.getElementById('reset-button');
 const statsText = document.getElementById('last-set-stats-text');
@@ -18,7 +21,6 @@ const deleteTodayStatsButton = document.getElementById('delete-today-stats');
 const deleteTotalStatsButton = document.getElementById('delete-total-stats');
 
 let paragraphWithText = document.getElementById('text');
-
 let charArray = [];
 let charIndex = 0;
 let seconds = 0;
@@ -33,7 +35,63 @@ statsText.innerHTML = displayStats(0, 0);
 todayStatsText.innerHTML = displayTodayStats();
 totalStatsText.innerHTML = displayTotalStats();
 
+let keyDownHandler = async function(event) {
+  const userInput = event.code;
+  const userInputIsCorrect = charArray[charIndex] == event.key;
+
+  keyboardKeysArray.forEach(function(entryElement) {
+    console.log(keyboardKeysArrayEntriesIterator.next().value);
+  });
+
+  if (generateTextButtonIsClicked) {
+
+    if (!userInputIsCorrect) {
+      userMistakesCount += 1;
+      localStorage.todayMistypes = parseInt(localStorage.todayMistypes) + 1;
+      localStorage.totalMistypes = parseInt(localStorage.totalMistypes) + 1;
+    }
+
+    if (userInput != 'Space') {
+      userKeyTypeCount += 1;
+      localStorage.todayCharsTyped = parseInt(localStorage.todayCharsTyped) + 1;
+      localStorage.totalCharsTyped = parseInt(localStorage.totalCharsTyped) + 1;
+    }
+
+    const endOfArrayIsReached = charIndex == charArray.length - 1;
+    if (endOfArrayIsReached) {
+      charArray = await generateText(paragraphWithText);
+      resetProgress();
+      localStorage.todayAmountOfSets = parseInt(localStorage.todayAmountOfSets) + 1;
+      localStorage.totalAmountOfSets = parseInt(localStorage.totalAmountOfSets) + 1;
+
+      return;
+    }  
+
+    charIndex = handleKeyDownEvent(event, paragraphWithText, statsText, todayStatsText, charArray, charIndex, seconds, userKeyTypeCount, userMistakesCount);
+  }
+}
+
+function resetProgress() {
+  charIndex = 0;
+  seconds = 0;
+  userKeyTypeCount = 0;
+  userMistakesCount = 0;
+  paragraphWithText.innerHTML = markCurrentChar(paragraphWithText, charIndex);
+  statsText.innerHTML = displayStats(userMistakesCount, userKeyTypeCount, seconds);
+  todayStatsText.innerHTML = displayTodayStats(userKeyTypeCount);
+}
+
+function initializeFrequencyEntries() {
+  keyboardKeysArray.forEach(function() {
+    for (let index of keyboardKeysArrayEntriesIterator) {
+      index = 0;
+    }
+  });
+}
+
 generateTextButton.addEventListener('click', async function() {
+  initializeFrequencyEntries();
+
   if (generateTextButtonIsClicked) {
       resetProgress();
       document.removeEventListener('keydown', keyDownHandler);
@@ -66,45 +124,6 @@ generateTextButton.addEventListener('click', async function() {
 resetProgressButton.addEventListener('click', function() {
   resetProgress();
 });
-
-let keyDownHandler = async function(event) {
-  const userInputIsCorrect = charArray[charIndex] == event.key;
-  if (generateTextButtonIsClicked) {
-    if (!userInputIsCorrect) {
-      userMistakesCount += 1;
-      localStorage.todayMistypes = parseInt(localStorage.todayMistypes) + 1;
-      localStorage.totalMistypes = parseInt(localStorage.totalMistypes) + 1;
-    }
-
-    if (event.code != 'Space') {
-      userKeyTypeCount += 1;
-      localStorage.todayCharsTyped = parseInt(localStorage.todayCharsTyped) + 1;
-      localStorage.totalCharsTyped = parseInt(localStorage.totalCharsTyped) + 1;
-    }
-
-    const endOfArrayIsReached = charIndex == charArray.length - 1;
-    if (endOfArrayIsReached) {
-      charArray = await generateText(paragraphWithText);
-      resetProgress();
-      localStorage.todayAmountOfSets = parseInt(localStorage.todayAmountOfSets) + 1;
-      localStorage.totalAmountOfSets = parseInt(localStorage.totalAmountOfSets) + 1;
-
-      return;
-    }  
-
-    charIndex = handleKeyDownEvent(event, paragraphWithText, statsText, todayStatsText, charArray, charIndex, seconds, userKeyTypeCount, userMistakesCount);
-  }
-}
-
-function resetProgress() {
-  charIndex = 0;
-  seconds = 0;
-  userKeyTypeCount = 0;
-  userMistakesCount = 0;
-  paragraphWithText.innerHTML = markCurrentChar(paragraphWithText, charIndex);
-  statsText.innerHTML = displayStats(userMistakesCount, userKeyTypeCount, seconds);
-  todayStatsText.innerHTML = displayTodayStats(userKeyTypeCount);
-}
 
 deleteTodayStatsButton.addEventListener('click', resetLocalStorageForTodayStats);
 deleteTotalStatsButton.addEventListener('click', resetLocalStorageForTotalStats);
